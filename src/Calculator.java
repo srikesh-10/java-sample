be879b2f674ba6bf2c6e0972eea56f53fdfad800
Vaibhav <vaibhavshrivastava333@gmail.com>
2026-02-12 09:35:58 +0530

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double first, second;
+        String op;
+        System.out.print("Enter first number: ");
+        first = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        second = scanner.nextDouble();
+        System.out.print("Enter operator (+, -, *, /): ");
+        op = scanner.next();
+        double result;
+
+        switch (op) {
+            case "+":
+                result = first + second;
+                System.out.println("Result: " + result);
+                break;
+            case "-":
+                result = first - second;
+                System.out.println("Result: " + result);
+                break;
+            case "*":
+                result = first * second;
+                System.out.println("Result: " + result);
+                break;
+            case "/":
+                if (second != 0) {
+                    result = first / second;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid Operator");
+        }
+    }
+}
